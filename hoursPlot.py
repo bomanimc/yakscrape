9a189f266b2f0d8917d0781b4663d45fe839a281
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2015-12-06 20:33:21 -0600

    Edited time plots to account for lack of scraping across the day by binning

--- hoursPlot.py
+++ hoursPlot.py
@@ -9,17 +9,18 @@ client = MongoClient("mongodb://eecs395:395pw@ds027155.mongolab.com:27155/yakscr
 db = client['yakscrape']
 yaks = db.yaks
 
-# NU = ['Northwestern University, Evanston, IL', 'Wilmette, IL', 'Skokie, IL', 'Rogers Park, IL']
-# UChicago = ['University of Chicago', 'Hyde Park, IL', 'Kenwood, IL', 'Southshore Park, IL']
-# Columbia = ['Columbia University', 'Manhattan, NY', 'Bronx, NY', 'North Bergen, NJ', 'Queens, NY', 'Brooklyn, NY']
-# Stanford = ['Stanford University', 'Los Altos, CA', 'Palo Alto, CA', 'Menlo Park, CA']
-# JHU = ['John Hopkins University', 'Hampden, MD', 'Remington, MD', 'Waverly, MD'];
-# Vanderbilt = ['Vanderbilt University', 'Green Hills, TN', 'East Nashville, TN', 'Berry Hill, TN']
-# Madison = ['University of Wisconsin - Madison', 'Madison, WI', 'Monona, WI', 'Fitchburg, WI']
-# CollegePark = ['University of Maryland - College Park', 'Adelphi, MD', 'Hyattsville, MD', 'Beywrn Heights, MD']
-# UCLA = ['University of Californa - Los Angeles', 'Bel Air, CA', 'Holmby Hills, CA', 'Century City, CA', 'Brentwood, CA']
-# WashU = ['Washington University St. Louis', 'Ferguson, MO', 'Central West End, MO', 'Downtown St. Louis']
-# Rice = ['Rice University','Downtown Houston', 'Montrose, TX', 'Woodland Heights. TX']
+NU = ['Northwestern University, Evanston, IL', 'Wilmette, IL', 'Skokie, IL', 'Rogers Park, IL']
+UChicago = ['University of Chicago', 'Hyde Park, IL', 'Southshore Park, IL']
+#Took out Kenwood from UChi
+Columbia = ['Manhattan, NY', 'Bronx, NY', 'North Bergen, NJ', 'Queens, NY', 'Brooklyn, NY']
+Stanford = ['Stanford University', 'Los Altos, CA', 'Palo Alto, CA', 'Menlo Park, CA']
+JHU = ['John Hopkins University', 'Hampden, MD', 'Remington, MD', 'Waverly, MD'];
+Vanderbilt = ['Vanderbilt University', 'Green Hills, TN', 'East Nashville, TN', 'Berry Hill, TN']
+Madison = ['University of Wisconsin - Madison', 'Madison, WI', 'Monona, WI', 'Fitchburg, WI']
+CollegePark = ['University of Maryland - College Park', 'Adelphi, MD', 'Hyattsville, MD', 'Beywrn Heights, MD']
+UCLA = ['University of Californa - Los Angeles', 'Bel Air, CA', 'Holmby Hills, CA', 'Century City, CA', 'Brentwood, CA']
+WashU = ['Washington University St. Louis', 'Ferguson, MO', 'Central West End, MO', 'Downtown St. Louis']
+Rice = ['Rice University','Downtown Houston', 'Montrose, TX', 'Woodland Heights. TX']
 
 #Function for clearly printing a dictionary's values
 def dictPrint(dictIn):
@@ -30,14 +31,20 @@ def dictPrint(dictIn):
 # A function for binning the times of each posts
 def timeBinning(cursor):
 	amount = cursor.count()
-	timeBins = dict.fromkeys(range(0, 24), 0)
+	#timeBins = dict.fromkeys(range(0, 24), 0)
+	timeBins = dict.fromkeys(range(0, 3), 0)
 	for row in cursor:
 		rowHour = parser.parse(row['time']).hour
-		timeBins[rowHour] += 1
+		if(rowHour > 1 and rowHour <= 8):  #Morning Bucket
+			timeBins[0] += 1
+		elif(rowHour > 8 and rowHour <= 16): 
+			timeBins[1] += 1
+		else:
+			timeBins[2] += 1
 	return(timeBins)
 	
 def normalize(dictIn, count):
-	normalBins = dict.fromkeys(range(0, 24), 0)
+	normalBins = dict.fromkeys(range(0, 3), 0)
 	for attribute, value in dictIn.items():
 		normalBins[attribute] = 100*(dictIn[attribute]/float(count))
 	dictPrint(normalBins)
@@ -63,30 +70,31 @@ def createTraces(locations):
 		print(place)
 		dbCursor = yaks.find({"region":place})
 		timeBins = normalize(timeBinning(dbCursor), dbCursor.count())
+		print(timeBins)
 
-		# Create traces
-		trace = go.Scatter(
-		    x = allHours,
-		    y = getVals(timeBins),
-		    mode = 'lines+markers',
-		    name = place,
-			opacity=0.75
+		trace = go.Bar(
+			x = ['2AM-8AM', '9AM-4PM', '5PM-1AM'],
+			y = getVals(timeBins), 
+			name=place,
+		    opacity=0.75
 		)
+		
 		data.append(trace)
-
 	return(data)
 
 
 layout = go.Layout(
-    title='New York Area Yak Activity',
+	title='Rice Area Yak Activity',
     xaxis=dict(
-        title='Time (Hour)'
+    	title='Time Range',
     ),
     yaxis=dict(
         title='Percent of Yaks'
-    )
+    ),
+    barmode='group',
 )
 
-area = ['Manhattan, NY', 'Bronx, NY', 'North Bergen, NJ', 'Queens, NY', 'Brooklyn, NY']
+
+area = Rice;
 fig = go.Figure(data=createTraces(area), layout=layout)
-plot_url = py.plot(fig, filename='time-line')
+plot_url = py.plot(fig, filename='time-bar')
